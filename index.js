df625a3e300b6b94686f5258bad617e797ed8a6c
Chotinat <67574610+Chotinat@users.noreply.github.com>
2023-03-02 21:35:01 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// import React from "React"
+import App from "./App.js"
+function Page(){
+  return(
+    <div>
+      <h1>Hello</h1>
+      <App.App />
+    </div>
+  )
+}
+
+
+ReactDOM.render(<Page />, document.getElementById("root"))
